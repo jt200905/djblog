aae10de64015a48a0bf678527a494d82f77654dd
jt200905 <jt200905@gmail.com>
2019-05-30 23:34:16 +0800

    第一將djblog 專案上傳到 Github

---
+++ README.md
@@ -0,0 +1,7 @@
+1.	開發環境:
+Python 版本 :3.7.3
+Django 版本 :2.2.1
+IDE :VS Code 1.33.1
+OS:Windows 7
+
+開發環境設定:參考 Visual Studio Code 開發 Django.docx
---
+++ blenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!j:\source\djblog\blenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
